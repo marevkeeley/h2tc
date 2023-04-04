37f032d539363877810374d929cb3d9864576193
Evelyn Jones <marevkeeley@gmail.com>
2023-04-04 13:35:09 -0400

    Added new images

--- src/Home.js
+++ src/Home.js
@@ -3,9 +3,12 @@ import { Link } from "react-router-dom";
 import { GiWindTurbine } from "react-icons/gi";
 import { GiFactory } from "react-icons/gi";
 import {BsBuildings} from 'react-icons/bs';
-import background from "./background.png";
+import map from "./map.png";
+import smallmap from "./smallmap.png";
 import {GiFamilyHouse} from "react-icons/gi";
 import {FaCarBattery} from 'react-icons/fa';
+import Popup from 'reactjs-popup';
+// import 'reactjs-popup/dist/index.css';
 
   const icon = {
     left: 812, 
@@ -20,132 +23,13 @@ import {FaCarBattery} from 'react-icons/fa';
     textDecoration: 'none',
   };
 
-  const icon1 = {
-    left: 780, 
-    top: 400,
-    backgroundColor: 'white', 
-    borderRadius: 10, 
-    padding: 2, 
-    textAlign: 'center', 
-    position: 'absolute', 
-    fontSize: 20,
-    underlined: 'false',
-    textDecoration: 'none',
-  };
-
-  const icon2 = {
-    left: 640, 
-    top: 265,
-    backgroundColor: 'white', 
-    borderRadius: 10, 
-    padding: 2, 
-    textAlign: 'center', 
-    position: 'absolute', 
-    fontSize: 20,
-    underlined: 'false',
-    textDecoration: 'none',
-  };
-
-  const icon3 = {
-    left: 780, 
-    top: 440,
-    backgroundColor: 'white', 
-    borderRadius: 10, 
-    padding: 2, 
-    textAlign: 'center', 
-    position: 'absolute', 
-    fontSize: 20,
-    underlined: 'false',
-    textDecoration: 'none',
-  };
-
-  const icon4 = {
-    left: 610, 
-    top: 235,
-    backgroundColor: 'white', 
-    borderRadius: 10, 
-    padding: 2, 
-    textAlign: 'center', 
-    position: 'absolute', 
-    fontSize: 20,
-    underlined: 'false',
-    textDecoration: 'none',
-  };
-
-  const icon5 = {
-    left: 663, 
-    top: 265,
-    backgroundColor: 'white', 
-    borderRadius: 10, 
-    padding: 2, 
-    textAlign: 'center', 
-    position: 'absolute', 
-    fontSize: 20,
-    underlined: 'false',
-    textDecoration: 'none',
-  };
-
-  const icon6 = {
-    left: 600, 
-    top: 200,
-    backgroundColor: 'white', 
-    borderRadius: 10, 
-    padding: 2, 
-    textAlign: 'center', 
-    position: 'absolute', 
-    fontSize: 20,
-    underlined: 'false',
-    textDecoration: 'none',
-  };
-
-  const icon7 = {
-    left: 610, 
-    top: 265,
-    backgroundColor: 'white', 
-    borderRadius: 10, 
-    padding: 2, 
-    textAlign: 'center', 
-    position: 'absolute', 
-    fontSize: 20,
-    underlined: 'false',
-    textDecoration: 'none',
-  };
-
-  const link1 = {
-    left: 380, 
-    top: 400,
+  const link = {
     backgroundColor: 'white', 
     borderRadius: 10, 
     padding: 5, 
     textAlign: 'center', 
     position: 'absolute', 
-    fontSize: 10,
-    underlined: 'false',
-    textDecoration: 'none',
-  };
-
-  const link2 = {
-    left: 380, 
-    top: 420,
-    backgroundColor: 'white', 
-    borderRadius: 10, 
-    padding: 5, 
-    textAlign: 'center', 
-    position: 'absolute', 
-    fontSize: 10,
-    underlined: 'false',
-    textDecoration: 'none',
-  };
-
-  const link3 = {
-    left: 380, 
-    top: 440,
-    backgroundColor: 'white', 
-    borderRadius: 10, 
-    padding: 5, 
-    textAlign: 'center', 
-    position: 'absolute', 
-    fontSize: 10,
+    fontSize: 20,
     underlined: 'false',
     textDecoration: 'none',
   };
@@ -153,23 +37,36 @@ import {FaCarBattery} from 'react-icons/fa';
 const Home = () =>{
     return (
       <div style={{
-        backgroundImage: `url(${background})`,
+        backgroundImage: `url(${map})`,
         backgroundRepeat: 'no-repeat',
         backgroundPosition: 'center',
-        scale: '100%'
+        backgroundSize: '95%',
       }}>
         <body className="App-header">
-          <Link to="/turbine" style={icon}><GiWindTurbine /></Link>
+          {/* <Link to="/turbine" style={icon}><GiWindTurbine /></Link>
           <Link to="/landing" style={icon1}><FaCarBattery /></Link>
           <Link to="/h2tcenter" style={icon2}><GiFamilyHouse /></Link>
           <Link to="/plant1" style={icon3}><GiFactory /></Link>
           <Link to="/plant2" style={icon4}><GiFactory /></Link>
           <Link to="/micro" style={icon5}><BsBuildings /></Link>
           <Link to="/dominion" style={icon6}><BsBuildings /></Link>
-          <Link to="/vng" style={icon7}><BsBuildings /></Link>
-          <Link to="/genplant" style={link1}>Genplant</Link>
-          <Link to="/siemens" style={link2}>Siemens</Link>
-          <Link to="/politicians" style={link3}>Politicians</Link>
+          <Link to="/vng" style={icon7}><BsBuildings /></Link> */}
+          <Popup 
+          trigger=
+            {<button style={{position: 'absolute', top: '65%', left:'80%', padding:20}}>Zoom in</button>}
+              modal nested>
+                {
+                  close => (
+                    <div>
+                      <img src={smallmap} style={{scale: '80%', position: 'relative', top: -60}}/>
+                      <button style={{top: '3%', left: '84%', position: 'absolute'}}onClick={() => {close()}}>Close</button>
+                    </div>
+                  )
+              }
+            </Popup>
+          <Link to="/genplant" style={Object.assign({left: '28%', top: '90%'}, link)}>Genplant</Link>
+          <Link to="/siemens" style={Object.assign({left: '45%', top: '90%'}, link)}>Siemens</Link>
+          <Link to="/politicians" style={Object.assign({left: '62%', top: '90%'}, link)}>Politicians</Link>
         </body>
       </div>
     );
