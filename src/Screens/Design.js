f5ec5289d752ae5edf847adbe8dd9ff222714224
Evelyn Jones <marevkeeley@gmail.com>
2023-05-02 11:17:44 -0400

    Added to design page.

--- src/Screens/Design.js
+++ src/Screens/Design.js
@@ -6,51 +6,78 @@ import double from "../assets/double.png";
  */
 const Design = () => {
   return (
-    <body
-      className="App-header"
-      style={{
-        backgroundImage: `url(${double})`,
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "center",
-        backgroundSize: "70%",
-        overflow: "hidden",
-      }}
-    >
-      <div
-        style={{
-          top: "38%",
-          left: "31%",
-          width: "20%",
-          position: "absolute",
-          textAlign: "center",
-        }}
-      >
-        <header style={{ fontWeight: "bold" }}>Project Plan</header>
+    <body className="App-header2">
+      <div style={{ flex: 1 }}>
         <a
           target="_blank"
           rel="noopener noreferrer"
-          href="https://docs.google.com/spreadsheets/d/1Nwk09iHT7JMrnE0WTv6JH-0sDVAOGWk3p2nwYszMngA/edit?usp=sharing"
+          href="https://docs.google.com/spreadsheets/d/1BHXrnV9Q8NwSeU_CAzdXHYmNjHjufmCdpbkDMp0c7KI/edit?usp=sharing"
         >
-          <p style={{ fontSize: 14 }}>H2 Project Plans</p>
+          <img
+            alt="dashboard"
+            src={require("../assets/dash.png")}
+            style={{ paddingLeft: 100, width: "80%" }}
+          />
         </a>
       </div>
       <div
         style={{
-          top: "58%",
-          left: "49%",
-          width: "20%",
-          position: "absolute",
-          textAlign: "center",
+          flex: 1,
+          backgroundImage: `url(${double})`,
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          backgroundSize: "70%",
+          overflow: "hidden",
         }}
       >
-        <header style={{ fontWeight: "bold" }}>Layout</header>
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://docs.google.com/document/d/1_Z1SwqcBUucsMn4SjjHpjRh6_quEgsfT/edit?usp=sharing&ouid=117891202262649805610&rtpof=true&sd=true"
+        <img
+          alt="background"
+          src={require("../assets/double.png")}
+          style={{ width: "100%", paddingRight: 100 }}
+        />
+        <div
+          style={{
+            top: "39%",
+            left: "59%",
+            width: "20%",
+            position: "absolute",
+            textAlign: "center",
+          }}
         >
-          <p style={{ fontSize: 14 }}>H2TC Layout</p>
-        </a>
+          <header style={{ fontWeight: "bold" }}>Project Plan</header>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://docs.google.com/spreadsheets/d/1Nwk09iHT7JMrnE0WTv6JH-0sDVAOGWk3p2nwYszMngA/edit?usp=sharing"
+          >
+            <p style={{ fontSize: 14 }}>H2 Project Plans</p>
+          </a>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="hhttps://docs.google.com/spreadsheets/d/1Nwk09iHT7JMrnE0WTv6JH-0sDVAOGWk3p2nwYszMngA/edit?usp=sharing"
+          >
+            <p style={{ fontSize: 14 }}>Sponsorship</p>
+          </a>
+        </div>
+        <div
+          style={{
+            top: "55%",
+            left: "72%",
+            width: "20%",
+            position: "absolute",
+            textAlign: "center",
+          }}
+        >
+          <header style={{ fontWeight: "bold" }}>Layout</header>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://docs.google.com/document/d/1_Z1SwqcBUucsMn4SjjHpjRh6_quEgsfT/edit?usp=sharing&ouid=117891202262649805610&rtpof=true&sd=true"
+          >
+            <p style={{ fontSize: 14 }}>H2TC Layout</p>
+          </a>
+        </div>
       </div>
     </body>
   );
--- src/Screens/Map.js
+++ src/Screens/Map.js
@@ -70,39 +70,39 @@ const Map = () => {
         <div style={Object.assign({ left: "82%", top: "12%" }, text)}>
           <p style={{ fontWeight: "bold" }}>Key</p>
         </div>
-         <img
-            src={require("../assets/1.png")}
-            alt="Turbine"
-            style={Object.assign({ left: "82%", top: "20%", width: '3%' }, icon)}
-          />
+        <img
+          src={require("../assets/1.png")}
+          alt="Turbine"
+          style={Object.assign({ left: "82%", top: "20%", width: "3%" }, icon)}
+        />
         <div style={Object.assign({ left: "86%", top: "18%" }, text)}>
           <p>Turbine</p>
         </div>
         <img
-            src={require("../assets/2.png")}
-            alt="Landing"
-            style={Object.assign({ left: "82%", top: "30%", width: '3%' }, icon)}
-          />
+          src={require("../assets/2.png")}
+          alt="Landing"
+          style={Object.assign({ left: "82%", top: "30%", width: "3%" }, icon)}
+        />
         <div style={Object.assign({ left: "86%", top: "29%" }, text)}>
           <p>Production</p>
         </div>
         <img
-            src={require("../assets/26.png")}
-            alt="H2TC Center"
-            style={Object.assign({ left: "82%", top: "40%", width: '3%' }, icon)}
-          />
+          src={require("../assets/smalllogo.png")}
+          alt="H2TC Center"
+          style={Object.assign({ left: "82%", top: "40%", width: "3%" }, icon)}
+        />
         <div style={Object.assign({ left: "86%", top: "39%" }, text)}>
           <p>H2 Tech Center</p>
         </div>
         <img
-            src={require("../assets/23.png")}
-            alt="Microgrid"
-            style={Object.assign({ left: "82%", top: "50%", width: '3%' }, icon)}
-          />
+          src={require("../assets/23.png")}
+          alt="Microgrid"
+          style={Object.assign({ left: "82%", top: "50%", width: "3%" }, icon)}
+        />
         <div style={Object.assign({ left: "86%", top: "49%" }, text)}>
           <p>MicroGrid</p>
         </div>
-  
+
         {/* <div
           onClick={() => filter()}
           style={Object.assign(
@@ -125,7 +125,7 @@ const Map = () => {
           <img
             src={require("../assets/1.png")}
             alt="Turbine"
-            style={{ width: "3%" }}
+            style={{ width: "2%" }}
           />
         </Link>
         <Link
@@ -135,87 +135,94 @@ const Map = () => {
           <img
             src={require("../assets/2.png")}
             alt="Landing"
-            style={{ width: "3%" }}
+            style={{ width: "2%" }}
           />
         </Link>
         <Link
           to="/h2tcenter"
-          style={Object.assign({ left: "-9%", top: "43%" }, icon2)}
+          style={Object.assign({ left: "26%", top: "43%" }, icon2)}
         >
           <img
-            src={require("../assets/26.png")}
+            src={require("../assets/smalllogo.png")}
             alt="H2TC Center"
-            style={{ width: "3%" }}
+            style={{ width: "16%", backgroundColor: "white" }}
           />
         </Link>
         <Link
           to="/micro"
-          style={Object.assign({ left: "-6%", top: "43%"  }, icon2)}
+          style={Object.assign({ left: "-6%", top: "43%" }, icon2)}
         >
           <img
             src={require("../assets/23.png")}
             alt="Microgrid"
-            style={{ width: "3%" }}
+            style={{ width: "2%" }}
           />
         </Link>
         <Link
           to="/dominion"
           style={Object.assign({ left: "15%", top: "33%" }, icon2)}
         >
-           <img
+          <img
             src={require("../assets/dominionIcon.png")}
             alt="Dominion"
-            style={{ width: "20%" }}
+            style={{ width: "10%" }}
           />
         </Link>
         <Link
           to="/vng"
           style={Object.assign({ left: "23%", top: "34%" }, icon2)}
         >
-           <img
+          <img
             src={require("../assets/vngIcon.jpg")}
             alt="Landing"
-            style={{ width: "16%" }}
+            style={{ width: "10%" }}
           />
         </Link>
         <Link
           to="/port"
-          style={Object.assign({ left: "34%", top: "82%" }, icon2)}
+          style={Object.assign({ left: "0%", top: "82%" }, icon2)}
         >
-           <img
-            src={require("../assets/portIcon.png")}
-            alt="Port of Virginia"
-            style={{ width: "20%" }}
-          />
+          <div>
+            <img
+              src={require("../assets/portIcon.png")}
+              alt="Port of Virginia"
+              style={{ width: "4%" }}
+            />
+            <img
+              src={require("../assets/2.png")}
+              alt="Production"
+              style={{ width: "2%" }}
+            />
+          </div>
         </Link>
         <Link
           to="/cox"
           style={Object.assign({ left: "20%", top: "80%" }, icon2)}
         >
-           <img
+          <img
             src={require("../assets/coxIcon.png")}
             alt="Cox"
-            style={{ width: "20%" }}
+            style={{ width: "10%" }}
           />
         </Link>
         <Link
           to="/jefferson"
           style={Object.assign({ left: "24%", top: "50%" }, icon2)}
         >
-           <img
+          <img
             src={require("../assets/jeffersonIcon.png")}
             alt="Jefferson Labs"
-            style={{ width: "20%" }}
+            style={{ width: "10%" }}
           />
         </Link>
         <Link
           to="/naval"
           style={Object.assign({ left: "36%", top: "73%" }, icon2)}
         >
-           <img
+          <img
             src={require("../assets/navalIcon.jpg")}
             alt="Naval Base"
-            style={{ width: "20%" }}
+            style={{ width: "10%" }}
           />
         </Link>
         {/* Extra links */}
