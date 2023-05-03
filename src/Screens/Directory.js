d9d0f76a82e71cf5e7265285c44b81507db4b24a
Evelyn Jones <marevkeeley@gmail.com>
2023-05-03 12:58:50 -0400

    Added directory

--- src/Screens/Directory.js
+++ src/Screens/Directory.js
@@ -1,13 +1,40 @@
+import hex from "../assets/Hex.png";
+
 /**
  * Creates a page to show the directory.
  * @returns the directory page.
  */
 const Directory = () => {
   return (
-    <body className="App-header">
-      <h1 style={{ paddingVertical: 5, fontSize: 18 }}>Contact Directory</h1>
-      <p>Coming soon</p>
-    </body>
+    <body
+    className="App-header"
+    style={{
+      backgroundImage: `url(${hex})`,
+      backgroundRepeat: "no-repeat",
+      backgroundPosition: "center",
+      backgroundSize: "24%",
+      overflow: "hidden",
+    }}
+  >
+    <div
+      style={{
+        top: "50%",
+        left: "40%",
+        width: "20%",
+        position: "absolute",
+        textAlign: "center",
+      }}
+    >
+      <header style={{ fontWeight: "bold" }}>Contact Directory</header>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://docs.google.com/spreadsheets/d/1aOcjR8Ge06XKuHEHuzo1wHKLtnE4qaO4o8--aCBVN6A/edit?usp=sharing"
+      >
+        <p style={{ fontSize: 14 }}>Directory</p>
+      </a>
+    </div>
+  </body>
   );
 };
 
